b9f98f78c9c57d83dd03a073926da477ad3d4033
Иванов Владислав <arshavin0309@mail.ru>
2022-03-30 22:35:43 +0500

    .

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 $( function() {
     $( "#accordion" ).accordion();
 } );
+
+// const questionHeading = document.querySelectorAll('.questions__heading')
+// const icon = document.querySelectorAll('.questions__heading::after')
+
+// questionHeading.forEach(e => {
+//     if (e.classList.contains('ui-accordion-icons')) {
+//         icon.classList.add('rotate')
+//     }
+// });
